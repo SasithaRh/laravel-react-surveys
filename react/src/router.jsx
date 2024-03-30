aa0bb74514e408fc243f2f89a42e6f0be32a62a5
SasithaRh <ruvishansth1030@gmail.com>
2024-03-30 10:36:09 +0530

    Add DefaultLayout

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,5 +1,5 @@
 import {createBrowserRouter, Navigate} from "react-router-dom";
-// import DefaultLayout from "./components/DefaultLayout.jsx";
+import DefaultLayout from "./components/DefaultLayout.jsx";
 import Dashboard from "./views/Dashboard.jsx";
 import Login from "./views/Login.jsx";
 import Surveys from "./views/Surveys.jsx";
@@ -7,24 +7,24 @@ import GuestLayout from "./components/GuestLayout.jsx";
 import Register from "./views/Register.jsx";
 
 const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <DefaultLayout/>,
-//     children: [
-//       {
-//         path: '',
-//         element: <Dashboard />
-//       },
-//       {
-//         path: '/dashboard',
-//         element: <Navigate to={'/'} />
-//       },
-//       {
-//         path: 'surveys',
-//         element: <Surveys />
-//       }
-//     ]
-//   },
+  {
+    path: "/",
+    element: <DefaultLayout/>,
+    children: [
+      {
+        path: '',
+        element: <Dashboard />
+      },
+      {
+        path: '/dashboard',
+        element: <Navigate to={'/'} />
+      },
+      {
+        path: '/surveys',
+        element: <Surveys />
+      }
+    ]
+  },
   {
     path: '/',
     element: <GuestLayout />,
