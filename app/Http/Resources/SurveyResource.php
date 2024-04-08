6f45eb44e7f7c5b56600f675c310e1ac192afe10
SasithaRh <ruvishansth1030@gmail.com>
2024-04-08 10:36:40 +0530

    Add the pagnation to surveys page

--- app/Http/Resources/SurveyResource.php
+++ app/Http/Resources/SurveyResource.php
@@ -19,13 +19,13 @@ class SurveyResource extends JsonResource
             'id' => $this->id,
             'title' => $this->title,
             'slug' => $this->slug,
-            // 'image_url' => $this->image ? URL::to($this->image) : null,
-            // 'status' => !!$this->status,
-            // 'description' => $this->description,
-            // 'created_at' => $this->created_at->format('Y-m-d H:i:s'),
-            // 'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
-            // 'expire_date' => (new \DateTime($this->expire_date))->format('Y-m-d'),
-            // 'questions' => SurveyQuestionResource::collection($this->questions)
+            'image_url' => $this->image ? URL::to($this->image) : null,
+            'status' => !!$this->status,
+            'description' => $this->description,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+            'expire_date' => (new \DateTime($this->expire_date))->format('Y-m-d'),
+            'questions' => SurveyQuestionResource::collection($this->questions)
         ];
     }
 }
